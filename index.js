a2f1396dc3b7192566a813cf90f6ce00529bd343
Strafwerkje <66158426+Strafwerkje@users.noreply.github.com>
2020-05-30 10:35:55 +0200

    init
    init

---
+++ index.js
@@ -0,0 +1,36 @@
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+ 
+const client = new discord.Client();
+client.login(botConfig.token);
+ 
+client.on("ready", async () => {
+ 
+    console.log(`${client.user.username} is online.`);
+ 
+    client.user.setActivity("Servers", { type: "BUILD" });
+ 
+});
+ 
+ 
+client.on("message", async message => {
+ 
+    if(message.author.bot) return;
+ 
+    if(message.channel.type === "dm") return;
+ 
+    var prefix = botConfig.prefix;
+ 
+    var messageArray = message.content.split(" ");
+ 
+    var command = messageArray[0];
+ 
+    if (command === `${prefix}hallo`) {
+ 
+        return message.channel.send("Hallo!!");
+   
+    }
+ 
+});
+
+bot.login(process.env.token);
